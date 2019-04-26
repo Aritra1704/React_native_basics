6af7c6483fe77d81bc184406ac29ea7780c785ea
Aritra <aritra1704@gmail.com>
2019-04-26 20:07:14 +0530

    ScrollView

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import ButtonBasics from './ButtonBasics';
+import ScrollviewSample from './ScrollviewSample';
 import {name as appName} from './app.json';
 
 
 
-AppRegistry.registerComponent(appName, () => ButtonBasics);
+AppRegistry.registerComponent(appName, () => ScrollviewSample);
